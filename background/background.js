dd3bf442bea1af95c7e59c6a59394883f766a694
Cyprian Nosek <cnosek@ioki.com.pl>
2017-06-04 19:53:30 +0200

    Remove unnecesary script

--- background/background.js
+++
@@ -1,7 +0,0 @@
-console.log("DUPA");
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("Here we go, we have a click");
-  var i = 1;
-  chrome.extension.currentTab.postMessage("get-ratings");
-});
